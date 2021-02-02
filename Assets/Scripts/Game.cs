5e02793bac554334e39c25510fe1202ff48a64a6
Penjat <theSpencerSymington@Gmail.com>
2021-02-02 16:58:40 -0330

    Game with dependancy injection.

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class Game {
+    Player player;
+    HyperGrid hyperGrid;
+    public Game(Player player, HyperGrid hyperGrid) {
+        this.player = player;
+        this.hyperGrid = hyperGrid;
+    }
+}
