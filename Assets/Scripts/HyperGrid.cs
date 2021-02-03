0c33ec80d08351df5c67df55d47b59028e95f42c
Penjat <theSpencerSymington@Gmail.com>
2021-02-03 10:51:03 -0330

    saftey for hypergrid.

--- Assets/Scripts/HyperGrid.cs
+++ Assets/Scripts/HyperGrid.cs
@@ -13,6 +13,14 @@ public class HyperGrid {
     }
 
     public bool checkBlocked(int x, int y, int z, int w) {
+        if(x >= grid.GetLength(0) || y >= grid.GetLength(1) || z >= grid.GetLength(2) || w >= grid.GetLength(3)){
+            return false;
+        }
+
+        if(x < 0 || y < 0 || z < 0 || w < 0) {
+            return false;
+        }
+
         return grid[x, y, z, w] != 0;
     }
 
