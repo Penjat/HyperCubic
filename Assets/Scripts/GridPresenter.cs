973139e9e2c97d6245d84be691c7d3a77837a452
Penjat <theSpencerSymington@Gmail.com>
2021-02-03 15:46:53 -0330

    Create 10x10x10 grid.

--- Assets/Scripts/GridPresenter.cs
+++ Assets/Scripts/GridPresenter.cs
@@ -3,8 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GridPresenter : MonoBehaviour {
+    private struct Constants {
+        public static float gridSpacing = 1.2f;
+    }
+
     public GameObject blockPrefab;
+
     public void createGrid() {
-        GameObject block = Instantiate(blockPrefab);
+        for(int x=0;x<10;x++){
+            for(int y=0;y<10;y++){
+                for(int z=0;z<10;z++){
+                GameObject block = Instantiate(blockPrefab);
+                blockPrefab.transform.position = new Vector3(x*Constants.gridSpacing,y*Constants.gridSpacing,z*Constants.gridSpacing);
+                }
+            }
+        }
     }
 }
