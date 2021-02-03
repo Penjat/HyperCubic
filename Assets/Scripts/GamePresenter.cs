484a3e97ebf34c607b82a6caf2c8f271ab194e88
Penjat <theSpencerSymington@Gmail.com>
2021-02-03 13:06:52 -0330

    Grid presenter.

---
+++ Assets/Scripts/GamePresenter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePresenter : MonoBehaviour {
+    Game game;
+    Player player;
+    HyperGrid hyperGrid;
+
+    void Start() {
+        Debug.Log("Starting up.");
+    }
+
+    void Update() {
+
+    }
+}
---
+++ Assets/Scripts/GridPresenter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPresenter : MonoBehaviour {
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
